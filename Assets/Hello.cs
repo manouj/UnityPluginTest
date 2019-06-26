c28dea2783c07156e952b230c28e8282c07a5465
Manouj Kumar <mxg1610@miami.edu>
2019-06-26 09:52:55 -0400

    Revert "HelloScript2"
    This reverts commit 41d16a39430acb40ae7b783480d2294e479c0059.

--- Assets/Hello.cs
+++ Assets/Hello.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloScritp2 : MonoBehaviour
+public class Hello : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
